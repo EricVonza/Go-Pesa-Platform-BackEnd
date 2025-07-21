ae95c08e5979c5f1a5d7136a65695777ea906875
Eric Vonza <ericvonza47@gmail.com>
2025-07-21 08:03:44 +0300

    Changed PORT to 5001

--- src/app.js
+++ src/app.js
@@ -33,5 +33,5 @@ mongoose
 app.use('/api/auth', authRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
